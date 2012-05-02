5905b01604b86e4a937d7f3264d69e58b50c9383
Sebastian Kurfuerst <sebastian@typo3.org>
2012-05-02 18:10:24 +0200

    initial commit

---
+++ Classes/Package.php
@@ -0,0 +1,15 @@
+<?php
+namespace Symfony\Component\DomCrawler;
+
+use TYPO3\FLOW3\Package\Package as BasePackage;
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Package base class of the Symfony.Component.DomCrawler package.
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class Package extends BasePackage {
+
+}
+?>
